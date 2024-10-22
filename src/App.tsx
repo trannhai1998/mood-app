67bd1e3f135d7b26a89c62738e3fea52f32f0c93
trannamhai98 <htran@atomiton.com>
2024-10-22 22:31:52 +0700

    Toggle thêm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import ToggleTheme from './components/toggle-theme';
+
+const darkTheme = createTheme({
+	
+	colorSchemes: {
+		dark: true,
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<main>This app is using the dark mode</main>
+			<ToggleTheme></ToggleTheme>
+		</ThemeProvider>
+	);
 }
 
 export default App;
